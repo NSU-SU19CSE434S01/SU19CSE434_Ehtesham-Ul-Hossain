f595cf3aa6e8547ecbd92dcc8c7462a96e44c036
Ehtesham <ahtesham.rafez@northsouth.edu>
2019-08-31 12:30:25 +0600

    LaunchApplication JAVA

---
+++ Project02/SeleniumDemo/src/demo/LaunchApplication.java
@@ -0,0 +1,16 @@
+package demo;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LaunchApplication {
+
+	public static void main(String[] args) {
+
+			System.setProperty("webdriver.chrome.driver", ".\\Drivers\\chromedriver.exe");
+			WebDriver driver = new ChromeDriver();
+			
+			driver.get("https://phptravels.com/demo/");
+	}
+
+}
