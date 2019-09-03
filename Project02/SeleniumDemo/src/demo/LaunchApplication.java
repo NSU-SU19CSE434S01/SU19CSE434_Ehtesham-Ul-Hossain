13b6abc3e8016cf80df6ff3a430ede104c43cc9b
Ehtesham <ahtesham.rafez@northsouth.edu>
2019-09-03 10:08:28 +0600

    Invoke: Calling Functions

--- Project02/SeleniumDemo/src/demo/LaunchApplication.java
+++ Project02/SeleniumDemo/src/demo/LaunchApplication.java
@@ -29,3 +29,31 @@ public class LaunchApplication {
 		
 	}
 	
+public void invokeBrowser() {
+
+		
+		try {
+			System.setProperty("webdriver.chrome.driver", ".\\Drivers\\chromedriver.exe");
+			driver = new ChromeDriver();
+			driver.manage().deleteAllCookies();
+			driver.manage().window().maximize();
+			driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+			driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
+			
+			driver.get("https://www.phptravels.net");
+			
+			setInputField();
+			signupLogin();
+			logIn();
+			Thread.sleep(3000);
+			driver.close();
+			
+		} catch (Exception e) {
+			
+			e.printStackTrace();
+		}
+		
+		
+	
+	}
+	
